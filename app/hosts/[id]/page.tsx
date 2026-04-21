7c159a847824abb896044cd7f90e8925e909957a
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2026-04-21 18:57:22 +0100

    Update page.tsx

--- app/hosts/[id]/page.tsx
+++ app/hosts/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Link from '@/components/NoPrefetchLink'
 import { fetchHostById, type Host } from '../../../lib/cache'
 import HostDetailClient from '../../../components/HostDetailClient'
-export const runtime = 'edge';
+export const revalidate = 86400;
 
 type Props = {
   params: Promise<{ id: string }>
