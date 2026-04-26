95af9ced317e220ffb700b610540b1bf8f2512a3
ShadowGaming100 <shadow@freehosts.space>
2026-04-26 18:23:19 +0100

    fix slug to no longer remove "." in names

--- lib/slugify.ts
+++ lib/slugify.ts
@@ -13,7 +13,7 @@ export function slugify(name: string): string {
   return name
     .toLowerCase()
     .replace(/\s+/g, '-')
-    .replace(/[^a-z0-9-]/g, '')
+    .replace(/[^a-z0-9-.]/g, '')
     .replace(/-{2,}/g, '-')
     .replace(/^-+|-+$/g, '')
 }
